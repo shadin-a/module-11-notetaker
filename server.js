d4d83de113a18f2edaee34880d08ab0b72ed6015
Shadin Al-Arab <shadin.alarab@gmail.com>
2022-10-03 19:18:28 -0700

    all routes created

--- server.js
+++ server.js
@@ -1,35 +1,62 @@
+const { json } = require('express');
 const express = require('express');
 //const db = require('./db/db.json');
 const fs = require('fs');
-const uid = new ShortUniqueId({ length: 10 });
+//const uid = new ShortUniqueId({ length: 10 });
 
 const PORT = 3000;
 const app = express();
 
 //ROUTE FOR LANDING PAGE
 app.get('/', (req, res) => {
-    res.sendFile('/Develop/public/index.html');
+    res.sendFile(path.join(__dirname, '.Develop/public/index.html'));
 })
 
 //ROUTE FOR API/NOTES PAGE
 app.get('/notes', (req, res) => {
   fs.readFile('/Develop/db/db.json', function (err, data) {
-    let getNoteData = JSON.parse(data);
+    var getNoteData = json.parse(data);
     res.json(getNoteData)
   });
-    res.sendFile('/Develop/public/notes.html');
+    res.sendFile(path.join(__dirname, '/Develop/public/notes.html'));
 });
 
 //ROUTE TO POST NOTES
-app.post()
-// add an endpoint for saving and hook up to save icon
-// save icon writes to db and immediately appears in left column
+app.post('/api/notes', (req,res) => {
+    const{title, text} = req.body;
+    if (req.body){
+        const newSavedNote = {
+            title,
+            text,
+            id: uid(),
+        };
+        fs.readFile('/Develop/db/db.json', function (err, data) {
+            let parsedData = json.parse(data);
+            parsedData.push(newSavedNote)
+            fs.writeFile('/Develop/db/db.json', JSON.stringify(parsedData), (err) => {
+            err
+            ? console.error(err)
+            : console.log('data parsed successfuly') 
+            
+            }
+        )}
+    )}
+    res.sendFile(path.join(__dirname, '/Develop/public/index.html'));
+});
 
-// endpoint for looking at a specific note in right hand column
-
-//trash icon route for specific notes
-
-// some write icon stuff i dont really get yet, but its another endpoint
+//ROUTE TO DELETE NOTES
+app.delete('/api/notes/:id', (req, res) =>{
+    let unwantedID = req.params.id
+    fs.readFile('/Develop/db/db.json', function (err, data) {
+        let oldNotes = json.parse(data)
+        let newNotes = oldNotes.filter(object => object.id !== unwantedID);
+        fs.writeFile('./db/db.json', JSON.stringify(newNotes), (err) =>
+        err
+        ? console.error(err)
+        : res.json(newNotes)   
+        );
+    });
+})
 
 // PORT LISTENING ROUTE
 app.listen(PORT, () => {
