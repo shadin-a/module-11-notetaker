6c2948e833dab6a072899d6400a215301269e3e1
Shadin Al-Arab <shadin.alarab@gmail.com>
2022-10-11 18:15:57 -0700

    port syntax fixed

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const uid = new ShortUniqueId({ length: 10 });
 const path = require('path');
 
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 //MIDDLEWARE
@@ -32,7 +32,7 @@ app.get('/api/notes', (req,res) => {
       });
       });  
     
-    app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html'))
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html'))
     );
 
 //ROUTE TO POST NOTES
